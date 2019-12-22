0abd244ff8521e16b1cc861568775131307c30b4
ashin.mahat <ashin.mahat@palmmind.com>
2019-12-22 19:56:08 +0545

    asdas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { 
+  AuthGuardService as AuthGuard 
+} from '../app/auth/auth.guard'
+import { MainPageComponent } from './main-page/main-page.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+const routes: Routes = [
+  {
+    path:'',
+    component:MainPageComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'admin',
+    canActivate:[AuthGuard],
+    component:AdminComponent
+  }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    HeaderComponent,
+    LoginComponent,
+    MainPageComponent,
+    AdminComponent,
   ],
   imports: [
     BrowserModule,
