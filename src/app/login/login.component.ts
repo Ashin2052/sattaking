340e4328ceebf11ea3f79a939d6f8deb3cc37d98
ashin.mahat <ashin.mahat@palmmind.com>
2019-12-30 21:43:17 +0545

    dfas

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ServicesService } from '../services.service';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public jwtHelper: JwtHelperService
 
-  constructor() { }
+  loginError;
+  constructor(private Service:ServicesService,private router:Router) { 
 
+    this.jwtHelper = new JwtHelperService();
+
+  }
+  user={
+    userName:'',
+    passWord:''
+  };
   ngOnInit() {
+    this.routeChange()
   }
 
+  routeChange()
+  {
+    const token = JSON.parse(localStorage.getItem('token'));
+    if(token)
+    {
+      if(!this.jwtHelper.isTokenExpired(token.jwtToken))
+      {
+       this.router.navigateByUrl('login')
+     }
+    }
+     
+  }
+  login(user)
+  {
+    console.log(user,"user")
+    this.Service.login(user).subscribe((response:any)=>
+    {
+      if(response)
+      {    
+
+          //  console.log(response.userMap.id,"response")
+           localStorage.setItem('token', JSON.stringify(response));
+            this.router.navigateByUrl('')
+           return true;
+
+    }},(error)=>{
+      this.loginError="your username or password mismatch";
+      // console.log(this.loginError,"error")
+
+      
+    }
+    )
+  }
 }
--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
+  baseUrl=environment.baseUrl+'/rest/v1/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  login(user)
+  {
+    return this.httpClient.post(this.baseUrl+"login/",user);
+  }
 }
