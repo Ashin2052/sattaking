f712e67d7d07d59fcafb37aa6056133430ed5f53
ashin.mahat <ashin.mahat@palmmind.com>
2019-12-31 11:59:56 +0545

    adas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule, ToastContainerModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsComponent } from './forms/forms.component';
@@ -9,9 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AdminComponent } from './admin/admin.component';
-
-
-
+import {NgxPaginationModule} from 'ngx-pagination';
 import { ServicesService } from './services.service';
 import { HttpClientModule } from '@angular/common/http';
 import { JwtHelperService } from '@auth0/angular-jwt';
@@ -31,9 +29,17 @@ import { PlacelistformComponent } from './placelistform/placelistform.component'
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastrModule.forRoot({
+      timeOut: 6000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true
+      }),
+    ToastContainerModule,
+    NgxPaginationModule
   ],
   providers: [ JwtHelperService],
   bootstrap: [AppComponent
