193c98fd6ea38997bade552639248e7b8e8c3729
ashin.mahat <ashin.mahat@palmmind.com>
2019-12-31 13:26:38 +0545

    adas

--- src/app/services/place-service.service.ts
+++ src/app/services/place-service.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaceServiceService {
 
-  constructor() { }
+  baserUrl=environment.baseUrl+'/rest/v1/';
+  constructor(private httpClient: HttpClient) { }
+
+savePlace(place)
+{
+  return this.httpClient.post(this.baserUrl+"login/",place);
+
+}
+
+deletePlace()
+{
+
+}
+
+updatePlace()
+{
+
+}
+
+
+
+  getAllPlace()
+  {
+
+  }
+
+getParticularPlace()
+{
+
+}
+
 }
