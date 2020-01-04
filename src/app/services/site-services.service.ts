9122e80d72d552e3e8238df30bc32537c10684fe
ashin.mahat <ashin.mahat@palmmind.com>
2020-01-04 20:54:53 +0545

    fasd

--- src/app/services/site-services.service.ts
+++ src/app/services/site-services.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SiteServicesService {
 
-  constructor() { }
+  baseUrl=environment.baseUrl+'/rest/v1/site/';
+  sattaToken = JSON.parse(localStorage.getItem('sattaToken')).jwtToken
+  constructor(private httpClient: HttpClient) { }
+
+savesite(site)
+{
+  this.sattaToken = JSON.parse(localStorage.getItem('sattaToken')).jwtToken
+ console.log(this.baseUrl,site)
+  return this.httpClient.post(this.baseUrl,site,{
+    headers:new HttpHeaders({
+      Authorization:this.sattaToken
+    })
+  });
+}
+
+deletesite(id)
+{
+  this.sattaToken = JSON.parse(localStorage.getItem('sattaToken')).jwtToken
+
+  return this.httpClient.delete(this.baseUrl+id,{
+    headers:new HttpHeaders({
+      Authorization:this.sattaToken
+    })
+  });
+}
+
+updatesite(id,value)
+{
+  this.sattaToken = JSON.parse(localStorage.getItem('sattaToken')).jwtToken
+
+  return this.httpClient.put(this.baseUrl+id,value,{
+    headers:new HttpHeaders({
+      Authorization:this.sattaToken
+    })
+  });
+}
+
+
+
+  getAllsite()
+  {
+    this.sattaToken = JSON.parse(localStorage.getItem('sattaToken')).jwtToken
+
+    return this.httpClient.get(this.baseUrl,{
+      headers:new HttpHeaders({
+        Authorization:this.sattaToken
+      })
+    });
+  }
+
+getParticularsite(id)
+{
+  this.sattaToken = JSON.parse(localStorage.getItem('sattaToken')).jwtToken
+
+  return this.httpClient.get(this.baseUrl+id,{
+    headers:new HttpHeaders({
+      Authorization:this.sattaToken
+    })
+  });
+}
 }
--- src/app/sites-form/sites-form.component.ts
+++ src/app/sites-form/sites-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SiteModel } from './sites.model';
 import { SiteAttribute } from './setAttbr';
+import { SiteServicesService } from '../services/site-services.service';
+import { Router } from '@angular/router';
+
 
 SiteModel
 @Component({
@@ -10,11 +13,12 @@ SiteModel
 })
 export class SitesFormComponent implements OnInit {
   urlId;
-  allValueList=[];
+  allValueList=<any>[];
   siteModel=new SiteModel();
+  placeSameError:boolean=true;
   siteAttribute=new SiteAttribute()
- 
-  constructor( ) { }
+  placeList=["asdas","zcxz","adsasd","asd"]
+  constructor(private siteService:SiteServicesService,private router:Router ) { }
 
   ngOnInit() {
   }
@@ -52,6 +56,11 @@ export class SitesFormComponent implements OnInit {
     const editField = event.target.textContent;
     this.allValueList[i].value = editField;
   }
+  updatedPlace(event, i)
+  {
+    this.allValueList[i].value = event.srcElement.value
+
+  }
   deleteValue(i)
   {
     this.allValueList.splice(i, 1);
@@ -61,9 +70,35 @@ cancelSite()
 {
 
 }
+checKplace()
+{
+for(var i=0;i<this.allValueList.length;i++)
+{
+  for (var k = i+1; k < this.allValueList.length; k++) {
+    if (this.allValueList[i].place == this.allValueList[k].place) {
+     this.placeSameError=true;
+     console.log("place")
+      //do stuff
+    }
+}}
+}
 saveSite()
 {
 
+  this.checKplace()
+if(this.placeSameError)
+{  this.placeSameError=false;
+
+  return ;
+}
+if(this.siteModel.siteName )
+{
+  this.siteModel.siteValue=this.allValueList;
+this.siteService.savesite(this.siteModel).subscribe((res:any)=>
+{
+  console.log(res)
+})
+}
 }
 
 }
