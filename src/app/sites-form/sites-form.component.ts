a80c201e66b0deb04f16e91dfc1a5997d2e14c0f
ashin.mahat <ashin.mahat@palmmind.com>
2020-01-05 22:36:30 +0545

    fas

--- src/app/sites-form/sites-form.component.ts
+++ src/app/sites-form/sites-form.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SiteModel } from './sites.model';
 import { SiteAttribute } from './setAttbr';
 import { SiteServicesService } from '../services/site-services.service';
-import { Router } from '@angular/router';
+import { Router,ActivatedRoute } from '@angular/router';
+import {ToastContainerDirective, ToastrService } from 'ngx-toastr';
 
 
 SiteModel
@@ -15,14 +16,31 @@ export class SitesFormComponent implements OnInit {
   urlId;
   allValueList=<any>[];
   siteModel=new SiteModel();
-  placeSameError:boolean=true;
+  placeSameError:boolean=false;
   siteAttribute=new SiteAttribute()
   placeList=["asdas","zcxz","adsasd","asd"]
-  constructor(private siteService:SiteServicesService,private router:Router ) { }
+  @ViewChild(ToastContainerDirective, {static: false}) toastContainer: ToastContainerDirective;
+
+  constructor(private activateRoute:ActivatedRoute, private toastr:ToastrService,private siteService:SiteServicesService,private router:Router ) { }
 
   ngOnInit() {
+    this.urlId=this.activateRoute.snapshot.paramMap.get('id')
+    if(this.urlId && (typeof(this.urlId))!="undefined")
+{
+  this.getInfoForUpdate()
+}
+
+
   }
- 
+  getInfoForUpdate()
+  {
+    this.siteService.getParticularsite(this.urlId).subscribe((response:any)=>
+    {
+      this.siteModel.siteName=response.siteName;
+      this.allValueList=response.siteValue
+    })
+  }
+
   mode()
   {
     if(this.siteAttribute.place && this.siteAttribute.value)
@@ -32,7 +50,7 @@ export class SitesFormComponent implements OnInit {
     }
     else
     {
-
+    this.toastr.warning("Eneter all values properly")
     }
 
   }
@@ -58,7 +76,7 @@ export class SitesFormComponent implements OnInit {
   }
   updatedPlace(event, i)
   {
-    this.allValueList[i].value = event.srcElement.value
+    this.allValueList[i].place = event.srcElement.place
 
   }
   deleteValue(i)
@@ -72,8 +90,14 @@ cancelSite()
 }
 checKplace()
 {
+  if(this.allValueList.length<=1)
+  {
+    this.placeSameError=false;
+    return;
+  }
 for(var i=0;i<this.allValueList.length;i++)
 {
+
   for (var k = i+1; k < this.allValueList.length; k++) {
     if (this.allValueList[i].place == this.allValueList[k].place) {
      this.placeSameError=true;
@@ -95,12 +119,35 @@ chooMethod()
 }
 updateSite()
 {
+  // this.checKplace()
+if(this.placeSameError)
+{  this.placeSameError=false;
 
+  return ;
+}
+if(this.siteModel.siteName )
+{
+  this.siteModel.siteValue=this.allValueList;
+this.siteService.updatesite(this.urlId,this.siteModel).subscribe((res:any)=>
+{
+  console.log(res)
+  this.router.navigateByUrl('sites')
+  this.placeSameError=false;
+  this.allValueList=[];
+  this.siteAttribute=new SiteAttribute()
+  this.siteModel=new SiteModel()
+  this.toastr.success(" Sites Successfully updated")
+},err=>
+{
+  this.toastr.error(" Sites update unsuccessful")
+
+})
+}
 }
 saveSite()
 {
 
-  this.checKplace()
+  // this.checKplace()
 if(this.placeSameError)
 {  this.placeSameError=false;
 
@@ -111,12 +158,16 @@ if(this.siteModel.siteName )
   this.siteModel.siteValue=this.allValueList;
 this.siteService.savesite(this.siteModel).subscribe((res:any)=>
 {
+  this.router.navigateByUrl('sites')
   console.log(res)
   this.allValueList=[];
   this.siteAttribute=new SiteAttribute()
   this.siteModel=new SiteModel()
+  this.toastr.success(" Sites Successfully saved")
+  
 },err=>
 {
+  this.toastr.error(" Sites save unsuccessful")
 
 })
 }
--- src/app/sites/sites.component.ts
+++ src/app/sites/sites.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SiteServicesService } from '../services/site-services.service';
 
 @Component({
   selector: 'app-sites',
@@ -7,12 +8,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./sites.component.css']
 })
 export class SitesComponent implements OnInit {
-
-  constructor(private router:Router ) { }
+  allSiteList=[]
+  constructor(private siteService:SiteServicesService,private router:Router ) { }
 
   ngOnInit() {
-  }
+    this.getAllSite()
 
+  }
+getAllSite()
+{
+this.siteService.getAllsite().subscribe((response:any)=>
+{
+this.allSiteList=response;
+})
+}
 delete(id, i)
   {
 
@@ -20,5 +29,9 @@ delete(id, i)
   addSite()
   {
 this.router.navigateByUrl('siteAdd')
+  }
+  editPlace(list)
+  {
+    this.router.navigateByUrl('siteAdd/'+list._id)
   }
 }
